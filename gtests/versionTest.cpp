3cf0b78af21648ceb7fdbd670f655c360fb45f50
Malov Vladimir <vldmalov@gmail.com>
2020-07-25 16:56:49 +0300

    В мастере материалы занятий

--- gtests/versionTest.cpp
+++
@@ -1,9 +0,0 @@
-#include "gtest/gtest.h"
-#include "versionLib.h"
-
-TEST(test_version, test_valid_version)
-{
-	EXPECT_EQ(versionLib::getMajorVersion(), 0);
-	EXPECT_EQ(versionLib::getMinorVersion(), 0);
-	ASSERT_GT(versionLib::getPatchVersion(), 0);
-}
